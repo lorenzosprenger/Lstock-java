1d1e369d4c1c68349a99471325d95d2a7d580936
Lorenzo Vanlare Sprenger <lorenzovanlaresprenger@MacBook-Air-de-Lorenzo-2.local>
2026-01-19 18:30:29 -0300

    Product entity

---
+++ src/main/java/com/lorenzosprenger/Lstock/entities/Product.java
@@ -0,0 +1,50 @@
+package com.lorenzosprenger.Lstock.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    private UUID id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
