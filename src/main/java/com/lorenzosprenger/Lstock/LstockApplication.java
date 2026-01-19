a59fa6aeeae7a4c3cff95e6cf1d2fb73f3363a15
Lorenzo Vanlare Sprenger <lorenzovanlaresprenger@MacBook-Air-de-Lorenzo-2.local>
2026-01-19 18:13:40 -0300

    Project created

---
+++ src/main/java/com/lorenzosprenger/Lstock/LstockApplication.java
@@ -0,0 +1,13 @@
+package com.lorenzosprenger.Lstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lorenzosprenger/Lstock/LstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenzosprenger.Lstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
